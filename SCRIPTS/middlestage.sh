944b3afc9c4381328c6aa4453abdabe86b80134d
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-08-05 00:06:19 +0200

    Update middlestage.sh

--- SCRIPTS/middlestage.sh
+++ SCRIPTS/middlestage.sh
@@ -1,2 +1,4 @@
-sleep 3 
-nohup xterm -e 'sudo bash /home/SCRIPTS/stage2-autofs-config.sh' > /dev/null
+sleep 5
+x-terminal-emulator -e 'sudo bash /home/SCRIPTS/stage2-autofs-config.sh' > /dev/null
+echo -e "Press ENTER to exit"
+read -r
